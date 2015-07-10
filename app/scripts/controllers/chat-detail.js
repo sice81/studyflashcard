8a8a465680574ff438e8bd9a02ae297a33e899c8
jaeik7 <jaeikpark81@gmail.com>
2015-07-11 01:13:26 +0900

    탭 정리

--- app/scripts/controllers/chat-detail.js
+++
@@ -1,9 +0,0 @@
-define(['angular', 'app'], function (angular, app) {
-  'use strict';
-
-  app.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
-    console.log('ChatDetailCtrl');
-
-    $scope.chat = Chats.get($stateParams.chatId);
-  });
-});
