616110df33178b2cbf696c498077ef269a7c6a76
jaeik7 <jaeikpark81@gmail.com>
2015-07-12 15:41:53 +0900

    대시보드 정리

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -20,50 +20,11 @@ define(['angular', 'app', 'myfavorite-service'], function (angular, app) {
     };
   });
 
-  app.controller('ListCtrl', function ($scope) {
-    console.log('ListCtrl');
+  app.controller('Dashboard.ListCtrl', function ($scope) {
+    console.log('Dashboard.ListCtrl');
 
     $scope.shouldShowDelete = false;
     $scope.shouldShowReorder = false;
     $scope.listCanSwipe = true
-  });
-
-  app.controller('DashboardChartCtrl', function ($scope) {
-    console.log('DashboardChartCtrl');
-
-    $scope.config = {
-      title: 'Products',
-      tooltips: true,
-      labels: false,
-      mouseover: function () {
-      },
-      mouseout: function () {
-      },
-      click: function () {
-      },
-      legend: {
-        display: true,
-        //could be 'left, right'
-        position: 'right'
-      }
-    };
-
-    $scope.data = {
-      series: ['Sales', 'Income', 'Expense', 'Laptops', 'Keyboards'],
-      data: [{
-        x: "Laptops",
-        y: [100, 500, 0],
-        tooltip: "this is tooltip"
-      }, {
-        x: "Desktops",
-        y: [300, 100, 100]
-      }, {
-        x: "Mobiles",
-        y: [351]
-      }, {
-        x: "Tablets",
-        y: [54, 0, 879]
-      }]
-    };
   });
 });
