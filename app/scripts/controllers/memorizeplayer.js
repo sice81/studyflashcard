25a535172f45848025ec523e31d680a146a5aa4e
Jaeik Park <jaeikpark81@gmail.com>
2015-07-13 09:54:17 +0900

    [memorize] range 연동 성공

--- app/scripts/controllers/memorizeplayer.js
+++ app/scripts/controllers/memorizeplayer.js
@@ -8,8 +8,8 @@ define(['angular', 'app', 'swiper', 'cardpacks-service'], function (angular, app
         if ($scope.$last === true) {
           var swiper = new Swiper('.swiper-container', {
             onTransitionEnd: function(swiper) {
-              console.log('onTransitionEnd', swiper.progress, $scope);
-              $scope.$emit('onChangeProgress', swiper.progress);
+              console.log('onTransitionEnd', swiper);
+              $scope.$emit('onChangeProgress', swiper.activeIndex);
             }
           });
         }
@@ -20,17 +20,18 @@ define(['angular', 'app', 'swiper', 'cardpacks-service'], function (angular, app
   app.controller('MemorizePlayerCtrl', function ($scope, $stateParams, Cardpacks) {
     console.log('MemorizePlayerCtrl');
 
-    $scope.$on('onChangeProgress', function(event, progree){
-      console.log('onChangeProgress', progree);
-      $scope.data.current = progree;
+    $scope.$on('onChangeProgress', function(event, index){
+      console.log('onChangeProgress', index);
+      $scope.data.current = index;
+      $scope.$apply();
     });
 
     $scope.data = {};
-    $scope.data.max = 10;
+    $scope.data.cardpack = Cardpacks.get($stateParams.cardPackId);
+    $scope.data.max = $scope.data.cardpack.cards.length-1;
     $scope.data.current = 0;
     $scope.data.right = 0;
     $scope.data.wrong = 0;
-    $scope.data.cardpack = Cardpacks.get($stateParams.cardPackId);
 
     $scope.right = function () {
       $scope.data.right++;
